2ed5d1269e4eb68bc5bd6df7d5c108a7dcfbf44f
mazima <#wlsdn6307>
2020-04-21 15:43:23 +0900

    commit

--- Notice/src/main/java/reservationBook/PolicyHandler.java
+++ Notice/src/main/java/reservationBook/PolicyHandler.java
@@ -15,28 +15,28 @@ public class PolicyHandler{
     public void wheneverReservationCanceled_Notice(@Payload ReservationCanceled reservationCanceled){
 
         if(reservationCanceled.isMe()){
-            System.out.println("##### listener Notice : " + reservationCanceled.toJson());
+            System.out.println("##### Reservation Canceled : " + reservationCanceled.toJson());
         }
     }
     @StreamListener(KafkaProcessor.INPUT)
     public void wheneverReturned_Notice(@Payload Returned returned){
 
         if(returned.isMe()){
-            System.out.println("##### listener Notice : " + returned.toJson());
+            System.out.println("##### Book Returned : " + returned.toJson());
         }
     }
     @StreamListener(KafkaProcessor.INPUT)
     public void wheneverRented_Notice(@Payload Rented rented){
 
         if(rented.isMe()){
-            System.out.println("##### listener Notice : " + rented.toJson());
+            System.out.println("##### Book Rented : " + rented.toJson());
         }
     }
     @StreamListener(KafkaProcessor.INPUT)
     public void wheneverReserved_Notice(@Payload Reserved reserved){
 
         if(reserved.isMe()){
-            System.out.println("##### listener Notice : " + reserved.toJson());
+            System.out.println("##### Book Reserved : " + reserved.toJson());
         }
     }
 
